806ccdb534b1bbf410c137743ee1e3589c0c3eab
Allan_PC <Allan.FL.15@live.com>
2021-02-05 13:57:23 -0600

    Mejore la forma de mostrar los iconos y mensajes al validar y agregue un feedback diferente para la validacion de formatos en el HTML

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,108 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.validate-form');
   const inputs = document.querySelectorAll('.form-input:not(.custom)');
-  const customInputs = document.querySelectorAll('.custom');
-  const customPhone = document.querySelector('.custom.phone');
+  const phoneInputs = document.querySelectorAll('.form-input.custom.phone');
   const cleavePhone = new Cleave('.form-input.custom.phone', {
     phone: true,
     delimiter: '-',
     phoneRegionCode: 'MX',
   });
 
-  function createIconElements(e) {
-    let good, wrong;
-    const wrongIcon = e.target.parentElement.querySelector('.fas.fa-times');
-    if (e.target.value === '' && wrongIcon) {
-      good = document.createElement('i');
-      good.className = 'fas fa-check';
-      e.target.parentElement.appendChild(good);
-    } else if (e.target.value === '') {
-      good = document.createElement('i');
-      good.className = 'fas fa-check';
-      e.target.parentElement.appendChild(good);
+  // Cargar iconos de aceptacion y error para cada input
+  document.querySelectorAll('.form-input').forEach((input) => {
+    const errorIcon = document.createElement('i');
+    errorIcon.className = 'fas fa-times';
+    const goodIcon = document.createElement('i');
+    goodIcon.className = 'fas fa-check';
 
-      wrong = document.createElement('i');
-      wrong.className = 'fas fa-times';
-      e.target.parentElement.appendChild(wrong);
-    }
-  }
-  function showCheckedIcon(e, option) {
-    let good;
-    let wrong;
-    if (e.target) {
-      good = e.target.parentElement.querySelector('.fas.fa-check');
-      wrong = e.target.parentElement.querySelector('.fas.fa-times');
-    }
-    if (option === 'good') {
-      good.classList.add('checked');
-    } else if (option === 'wrong') {
-      wrong.classList.add('checked');
-    }
-  }
-  function removeCheckedIcon(e, option) {
-    let good;
-    let wrong;
-    if (e.target) {
-      good = e.target.parentElement.querySelector('.fas.fa-check');
-      wrong = e.target.parentElement.querySelector('.fas.fa-times');
-    }
+    input.parentElement.appendChild(errorIcon);
+    input.parentElement.appendChild(goodIcon);
+  });
 
-    if (option === 'good') {
-      good.classList.remove('checked');
-    } else if (option === 'wrong') {
-      wrong.classList.remove('checked');
-    }
-  }
-  function removeGood(e) {
-    const good = e.target.parentElement.querySelector('.fas.fa-check');
-    good.remove();
-  }
-  function removeWrong(e) {
-    const wrong = e.target.parentElement.querySelector('.fas.fa-times');
-    wrong.remove();
-  }
-
+  // Cada input se validara de la misma forma al escribir y salir del input
   inputs.forEach((input) => {
-    input.addEventListener('focus', (e) => createIconElements(e));
-
-    input.addEventListener('blur', (e) => {
-      if (e.target) {
-        if (e.target.value === '') {
-          removeGood(e);
-          showCheckedIcon(e, 'wrong');
-        }
-      }
-    });
-    input.addEventListener('input', (e) => {
-      if (e.target.value !== '') {
-        removeCheckedIcon(e, 'wrong');
-        showCheckedIcon(e, 'good');
-      } else {
-        removeCheckedIcon(e, 'good');
-        showCheckedIcon(e, 'wrong');
-      }
-    });
+    input.addEventListener('blur', validateEmpty);
+    input.addEventListener('input', validateEmpty);
   });
 
-  customInputs.forEach((custom) => {
-    custom.addEventListener('focus', (e) => createIconElements(e));
-    custom.addEventListener('blur', (e) => {
-      if (e.target) {
-        if (e.target.value === '') {
-          removeGood(e);
-          removeWrong(e);
-        }
-      }
-    });
+  phoneInputs.forEach((input) => {
+    input.addEventListener('blur', validatePhoneFormat);
+    input.addEventListener('input', validatePhoneFormat);
   });
 
-  customPhone.addEventListener('input', (e) => {
-    if (/\d{2}-(\d{4}-)(\d{4})/g.test(e.target.value)) {
-      removeCheckedIcon(e, 'wrong');
-      showCheckedIcon(e, 'good');
-    } else {
-      removeCheckedIcon(e, 'good');
-      showCheckedIcon(e, 'wrong');
+  // Valida el contenido del input para mostrar u ocultar los iconos y mensajes de feedback
+  /**
+   * @param {Event} e
+   */
+  function validateEmpty(e) {
+    const error = e.target.parentElement.querySelector('.fa-times');
+    const good = e.target.parentElement.querySelector('.fa-check');
+    const validFeedback = e.target.parentElement.querySelector('.feedback .input-valid');
+    const invalidFeedback = e.target.parentElement.querySelector('.feedback .input-invalid');
+
+    if (e.target.value === '') {
+      good.classList.remove('show');
+      error.classList.add('show');
+      if (validFeedback) validFeedback.classList.remove('show');
+      if (invalidFeedback) invalidFeedback.classList.add('show');
+    } else if (e.target.value !== '') {
+      error.classList.remove('show');
+      good.classList.add('show');
+      if (invalidFeedback) invalidFeedback.classList.remove('show');
+      if (validFeedback) validFeedback.classList.add('show');
     }
-  });
+  }
+
+  // Validar el formato del input para mostrar u ocultar los iconos y mensaje de feedback
+  /**
+   * @param {Event} e
+   */
+  function validatePhoneFormat(e) {
+    const error = e.target.parentElement.querySelector('.fa-times');
+    const good = e.target.parentElement.querySelector('.fa-check');
+    const invalidFormatFeedback = e.target.parentElement.querySelector('.feedback .invalid-format');
+
+    if (/\d{2}-(\d{4}-)(\d{4})/g.test(e.target.value)) {
+      error.classList.remove('show');
+      good.classList.add('show');
+      if (invalidFormatFeedback) invalidFormatFeedback.classList.remove('show');
+    } else {
+      good.classList.remove('show');
+      error.classList.add('show');
+      if (invalidFormatFeedback) invalidFormatFeedback.classList.add('show');
+    }
+  }
 });
