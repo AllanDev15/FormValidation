bb3466d08f7cebc89a66b7ed166c08e3a4b1c687
Allan_PC <Allan.FL.15@live.com>
2021-02-21 12:32:22 -0600

    Agregue codigo que valida todos los inputs antes de enviar el formulario dando retroalimentacion al usuario en el boton

--- js/formValidation.js
+++ js/formValidation.js
@@ -10,8 +10,10 @@ if (userPrefersDark) {
   document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="css/dark.css">');
 }
 
+let inputsArray = [];
 document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.validate-form')) {
+    const submitBtn = document.querySelector('#submit');
     const inputs = document.querySelectorAll('.form-input:not(.custom)');
     const phoneInputs = document.querySelectorAll('.form-input.custom.phone');
     const cleavePhone = new Cleave('.form-input.custom.phone', {
@@ -19,7 +21,17 @@ document.addEventListener('DOMContentLoaded', () => {
       delimiter: '-',
       phoneRegionCode: 'MX',
     });
-    const correctInputs = true;
+
+    // Se crea un arreglo de objetos por cada input donde cada objeto tiene su id y una llave para identificar si es valido
+    const formInputs = document.querySelectorAll('.form-input');
+    formInputs.forEach((inp) => {
+      const infoInput = {
+        id: inp.id,
+        isValid: false,
+      };
+
+      inputsArray = [...inputsArray, infoInput];
+    });
 
     // Cada input se validara de la misma forma al escribir y salir del input
     inputs.forEach((input) => {
@@ -30,6 +42,25 @@ document.addEventListener('DOMContentLoaded', () => {
     phoneInputs.forEach((input) => {
       input.addEventListener('blur', validatePhoneFormat);
       input.addEventListener('input', validatePhoneFormat);
+    });
+
+    submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.target.classList.add('loading');
+      setTimeout(() => {
+        if (inputsArray.some((inp) => inp.isValid === false)) {
+          e.target.classList.add('invalid');
+          e.target.querySelector('span').innerText = 'Datos Incorrectos';
+          e.target.classList.remove('loading');
+        } else {
+          e.target.classList.add('valid');
+          e.target.querySelector('span').innerText = '\u2713 Todo bien!';
+          e.target.classList.remove('loading');
+          setTimeout(() => {
+            window.location.href = 'index.html';
+          }, 800);
+        }
+      }, 800);
     });
   }
 
@@ -49,21 +80,10 @@ document.addEventListener('DOMContentLoaded', () => {
    * @param {Event} e
    */
   function validateEmpty(e) {
-    const error = e.target.parentElement.querySelector('.fa-times');
-    const good = e.target.parentElement.querySelector('.fa-check');
-    const validFeedback = e.target.parentElement.querySelector('.feedback .input-valid');
-    const invalidFeedback = e.target.parentElement.querySelector('.feedback .input-invalid');
-
     if (e.target.value === '') {
-      good.classList.remove('show');
-      error.classList.add('show');
-      if (validFeedback) validFeedback.classList.remove('show');
-      if (invalidFeedback) invalidFeedback.classList.add('show');
+      invalid(e);
     } else if (e.target.value !== '') {
-      error.classList.remove('show');
-      good.classList.add('show');
-      if (invalidFeedback) invalidFeedback.classList.remove('show');
-      if (validFeedback) validFeedback.classList.add('show');
+      valid(e);
     }
   }
 
@@ -72,22 +92,41 @@ document.addEventListener('DOMContentLoaded', () => {
    * @param {Event} e
    */
   function validatePhoneFormat(e) {
-    const error = e.target.parentElement.querySelector('.fa-times');
-    const good = e.target.parentElement.querySelector('.fa-check');
-    const invalidFormatFeedback = e.target.parentElement.querySelector('.feedback .invalid-format');
-
     if (/\d{2}-(\d{4}-)(\d{4})/g.test(e.target.value)) {
-      good.classList.add('show');
-      error.classList.remove('show');
-      if (invalidFormatFeedback) invalidFormatFeedback.classList.remove('show');
+      valid(e);
     } else {
-      error.classList.add('show');
-      good.classList.remove('show');
-      if (invalidFormatFeedback) invalidFormatFeedback.classList.add('show');
+      invalid(e);
     }
   }
 
-  // * Muestra un mensaje general de campos incorrectos
+  function valid(e) {
+    const error = e.target.parentElement.querySelector('.fa-times');
+    const good = e.target.parentElement.querySelector('.fa-check');
+    const invalidFormatFeedback = e.target.parentElement.querySelector('.feedback .input-invalid');
+    const validFormatFeedback = e.target.parentElement.querySelector('.feedback .input-valid');
+    good.classList.add('show');
+    error.classList.remove('show');
+    if (invalidFormatFeedback) invalidFormatFeedback.classList.remove('show');
+    if (validFormatFeedback) validFormatFeedback.classList.add('show');
+    // Cuando es valido se busca su id en los objetos y se actualiza su atributo isValid a verdadero
+    inputsArray.forEach((inp) => {
+      if (inp.id === e.target.id) inp.isValid = true;
+    });
+  }
+
+  function invalid(e) {
+    const error = e.target.parentElement.querySelector('.fa-times');
+    const good = e.target.parentElement.querySelector('.fa-check');
+    const invalidFormatFeedback = e.target.parentElement.querySelector('.feedback .input-invalid');
+    const validFormatFeedback = e.target.parentElement.querySelector('.feedback .input-valid');
+    good.classList.remove('show');
+    error.classList.add('show');
+    if (invalidFormatFeedback) invalidFormatFeedback.classList.add('show');
+    if (validFormatFeedback) validFormatFeedback.classList.remove('show');
+    inputsArray.forEach((inp) => {
+      if (inp.id === e.target.id) inp.isValid = false;
+    });
+  }
 
   // TODO por medio de una clase que se pueda elegir si la validacion se hace al escribir o solo al salir del input
 });
